a0d35f8c7333d417111f0881f9642f045ebd94af
许晶俞 <shanhaisilu@foxmail.com>
2022-07-12 17:15:06 +0800

    完善第七章，增加通过dat.gui修改振幅和频率的模块

--- 07-glsl入门/main/07-通过dat.gui修改频率和振幅.js
+++ 07-glsl入门/main/07-通过dat.gui修改频率和振幅.js
@@ -2,9 +2,9 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // 顶点着色器
-import rawVertexShader from "../shader/raw/vertex.glsl";
+import rawVertexShader from "../shader/07-raw/vertex.glsl";
 // 片元着色器
-import rawFragmentShader from "../shader/raw/fragment.glsl";
+import rawFragmentShader from "../shader/07-raw/fragment.glsl";
 
 import * as dat from "dat.gui";
 
