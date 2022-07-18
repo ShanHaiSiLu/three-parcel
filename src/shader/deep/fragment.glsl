faf6ee2226df81895ef3ce23445a8f9dfea17908
许晶俞 <shanhaisilu@foxmail.com>
2022-07-18 13:20:55 +0800

    增加噪声图像的demo

--- src/shader/deep/fragment.glsl
+++ src/shader/deep/fragment.glsl
@@ -44,8 +44,91 @@ float noise(in vec2 _st) {
         (d - b) * u.x * u.y;
 }
 
+//	Classic Perlin 2D Noise 
+//	by Stefan Gustavson
+//
+vec4 permute(vec4 x) {
+    return mod(((x * 34.0) + 1.0) * x, 289.0);
+}
+
+vec2 fade(vec2 t) {
+    return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
+}
+// 柏林噪声
+float cnoise(vec2 P) {
+    vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+    vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+    Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
+    vec4 ix = Pi.xzxz;
+    vec4 iy = Pi.yyww;
+    vec4 fx = Pf.xzxz;
+    vec4 fy = Pf.yyww;
+    vec4 i = permute(permute(ix) + iy);
+    vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
+    vec4 gy = abs(gx) - 0.5;
+    vec4 tx = floor(gx + 0.5);
+    gx = gx - tx;
+    vec2 g00 = vec2(gx.x, gy.x);
+    vec2 g10 = vec2(gx.y, gy.y);
+    vec2 g01 = vec2(gx.z, gy.z);
+    vec2 g11 = vec2(gx.w, gy.w);
+    vec4 norm = 1.79284291400159 - 0.85373472095314 * vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+    g00 *= norm.x;
+    g01 *= norm.y;
+    g10 *= norm.z;
+    g11 *= norm.w;
+    float n00 = dot(g00, vec2(fx.x, fy.x));
+    float n10 = dot(g10, vec2(fx.y, fy.y));
+    float n01 = dot(g01, vec2(fx.z, fy.z));
+    float n11 = dot(g11, vec2(fx.w, fy.w));
+    vec2 fade_xy = fade(Pf.xy);
+    vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+    float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+    return 2.3 * n_xy;
+}
+
 void main() {
-    // 1.渐变
-    float val = vUv.x/vUv.y;
-    gl_FragColor = vec4(val, val, val, 1.0);
+    // 1. 噪声实现烟雾、波纹的效果
+    // float strength = noise(vUv);
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 2. 噪声加强
+    // float strength = noise(vUv * 10.0);
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 3. 清晰化
+    // float strength = step(0.5, noise(vUv * 1000.0));
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 4. 柏林噪声
+    // float strength = cnoise(vUv * 10.0);
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 5. 清晰化
+    // float strength = abs(cnoise(vUv * 10.0));
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 6. 透明光路
+    // float strength = 1.0 - abs(cnoise(vUv * 10.0));
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 7. 加入时间参数，运动效果
+    // float strength = 1.0 - step(0.2, abs(cnoise(vUv * 10.0 + uTime)));
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 8. 加入时间参数，波纹效果
+    // float strength = 1.0 - sin(cnoise(vUv * 10.0) * 5.0 + uTime);
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 9. 锐化波纹
+    // float strength = step(0.1, sin(cnoise(vUv * 10.0) * 20.0 + uTime));
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 10. 使用混合函数混合颜色
+    vec3 basicColor = vec3(1.0, 0.0, 1.0);
+    vec3 vUvColor = vec3(vUv, 1.0);
+    float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0 + uTime));
+
+    vec3 mixClolr = mix(basicColor, vUvColor, strength);
+    gl_FragColor = vec4(mixClolr, 1.0);
 }
